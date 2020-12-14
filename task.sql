f024bbdeed6d86101f81bda7ea622fb2078647ea
SiriousHunter <46524538+SiriousHunter@users.noreply.github.com>
2020-12-14 10:37:12 +0300

    Add files via upload

---
+++ task.sql
@@ -0,0 +1,111 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Дек 14 2020 г., 10:30
+-- Версия сервера: 10.3.22-MariaDB
+-- Версия PHP: 7.1.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `task`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `items`
+--
+
+CREATE TABLE `items` (
+  `id` int(11) NOT NULL,
+  `created_at` int(11) NOT NULL,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `price` float(10,3) NOT NULL,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `items`
+--
+
+INSERT INTO `items` (`id`, `created_at`, `title`, `price`, `image`, `user`) VALUES
+(3, 1607925323, 'Notebook2', 5500.050, NULL, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `phone` varchar(13) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `phone`, `name`, `email`, `password`) VALUES
+(1, '+380xxxxxxxxx', 'Alex', 'alex@mail.com', 'qwert'),
+(2, '+380xxxxxxxxx', 'Alex', 'alex@mail.com', 'qwerty'),
+(3, '+380xxxxxxxxx', 'Alex', 'alex@mail.com', 'qwerty'),
+(4, '+380xxxxxxxxx', 'Alex', 'alex@mail.com', 'qwerty'),
+(5, '+380xxxxxxxxx', 'Alex', 'alex@mail.com', 'qwerty'),
+(6, '+380xxxxxxxxx', 'Alex', 'alex@mail.com', 'qwerty'),
+(7, '+380xxxxxxxxx', 'Alex', 'alex@mail.com', 'qwerty'),
+(8, '+380xxxxxxxxx', 'Alex', 'alex@mail.com', 'qwerty'),
+(9, '+380xxxxxxxxx', 'Alex', 'alex@mail.com', 'qwerty'),
+(10, '+380xxxxxxxxx', 'Alex', 'alex@mail.com', 'qwerty');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `items`
+--
+ALTER TABLE `items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
